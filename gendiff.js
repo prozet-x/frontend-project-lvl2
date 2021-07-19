a2a083e64f0b7fb15a1c91a9a13c3f62d9eca87a
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-19 06:24:56 -0700

    step 2 done

--- gendiff.js
+++ gendiff.js
@@ -3,10 +3,17 @@
 import { Command } from 'commander';
 const program = new Command();
 
-program
+program 
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
 
+/* It is for 2 step
+const options = program.opts();
+if (options.format === undefined) console.log('no format');
+else if (options.format === true) console.log('add format');
+else console.log(`add format type ${options.format}`);*/
 //if (!program.args.length) program.help();
