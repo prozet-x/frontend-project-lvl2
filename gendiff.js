1b54c9664853e3c36d642cde999fa1bdb416d8d7
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-28 07:10:52 -0700

    check 1

--- gendiff.js
+++ gendiff.js
@@ -2,20 +2,24 @@
 import { Command } from 'commander';
 import makeDiff from './src/diff.js';
 
+const makeDiffAndOut = (filepath1, filepath2, options) => {
+  const result = (options.format !== 'stylish')
+    && (options.format !== 'plain')
+    && (options.format !== 'json')
+    ? 'Bad formatter name!'
+    : makeDiff(filepath1, filepath2, options.format);
+  console.log(result);
+  return result;
+};
+
 const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => {
-    const result = (options.format !== 'stylish')
-    && (options.format !== 'plain')
-    && (options.format !== 'json')
-      ? 'Bad formatter name!'
-      : makeDiff(filepath1, filepath2, options.format);
-    console.log(result);
-    return result;
-  });
+  .action((filepath1, filepath2, options) => makeDiffAndOut(filepath1, filepath2, options));
 
 program.parse(process.argv);
+
+export default makeDiffAndOut;
