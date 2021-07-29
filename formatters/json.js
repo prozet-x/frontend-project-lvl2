eccd6cda7db01d528716143fb8ef544d65a17317
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-29 00:52:08 -0700

    ckeck 7

--- formatters/json.js
+++ formatters/json.js
@@ -1,16 +1,27 @@
+const makeStatus = (sym) => {
+  if (sym === '-') {
+    return 'deleted';
+  }
+  return sym === '+' ? 'added' : 'no changes';
+};
+
 export default (data) => {
   const makeAll = (arr, res) => {
     arr.forEach((elem) => {
-      const obj = {};
-      [, obj.name] = elem;
-      if (elem[0] === '-') {
+      const obj = {
+        name: elem[1],
+        status: makeStatus(elem[0]),
+        value: Array.isArray(elem[2]) ? makeAll(elem[2], []) : elem[2],
+      };
+      // [, obj[`name`]] = elem;
+      /* if (elem[0] === '-') {
         obj.status = 'deleted';
       } else if (elem[0] === '+') {
         obj.status = 'added';
       } else {
         obj.status = 'no changes';
-      }
-      obj.value = Array.isArray(elem[2]) ? makeAll(elem[2], []) : elem[2];
+      } */
+      // obj.value = Array.isArray(elem[2]) ? makeAll(elem[2], []) : elem[2];
       res.push(obj);
     });
     return res;
