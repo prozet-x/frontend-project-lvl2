0e4679715fe3739b86e9925607afacb4e256c251
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-29 00:59:15 -0700

    check 8

--- formatters/json.js
+++ formatters/json.js
@@ -5,7 +5,7 @@ const makeStatus = (sym) => {
   return sym === '+' ? 'added' : 'no changes';
 };
 
-export default (data) => {
+/* export default (data) => {
   const makeAll = (arr, res) => {
     arr.forEach((elem) => {
       const obj = {
@@ -13,15 +13,6 @@ export default (data) => {
         status: makeStatus(elem[0]),
         value: Array.isArray(elem[2]) ? makeAll(elem[2], []) : elem[2],
       };
-      // [, obj[`name`]] = elem;
-      /* if (elem[0] === '-') {
-        obj.status = 'deleted';
-      } else if (elem[0] === '+') {
-        obj.status = 'added';
-      } else {
-        obj.status = 'no changes';
-      } */
-      // obj.value = Array.isArray(elem[2]) ? makeAll(elem[2], []) : elem[2];
       res.push(obj);
     });
     return res;
@@ -29,4 +20,22 @@ export default (data) => {
 
   const res = makeAll(data, []);
   return JSON.stringify(res);
+}; */
+
+export default (data) => {
+  const makeAll = (arr) => {
+    const res = [];
+    arr.forEach((elem) => {
+      const obj = {
+        name: elem[1],
+        status: makeStatus(elem[0]),
+        value: Array.isArray(elem[2]) ? makeAll(elem[2]) : elem[2],
+      };
+      res.push(obj);
+    });
+    return res;
+  };
+
+  const result = makeAll(data);
+  return JSON.stringify(result);
 };
