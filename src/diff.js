4d3e966798a2b15708ea110c9d0a9a35b19fdaf6
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-29 04:06:45 -0700

    check 13

--- src/diff.js
+++ src/diff.js
@@ -4,52 +4,13 @@ import getFormattedResult from '../formatters/index.js';
 
 const objToArr = (obj) => {
   const keys = Object.keys(obj);
-  const res = keys.reduce((arr, key) => {
-    arr.push([' ', key, typeof obj[key] === 'object' ? objToArr(obj[key]) : obj[key]]);
-    return arr;
-  }, []);
+  const res = keys.reduce((arr, key) => [...arr, [' ', key, typeof obj[key] === 'object' ? objToArr(obj[key]) : obj[key]]], []);
   return res;
 };
 
 const getDiff = (filePath1, filePath2) => {
-  /* const getDiffs = (obj1, obj2) => {
-    const keys = _.uniq(_.concat(Object.keys(obj1), Object.keys(obj2))).sort();
-    const res = keys.reduce((arr, key) => {
-      const in1 = _.has(obj1, key);
-      const in2 = _.has(obj2, key);
-      if (in1 && in2) {
-        if ((typeof obj1[key] === 'object') && (obj1[key] !== null) && (typeof obj2[key] !== 'object')) {
-          arr.push(['-', key, objToArr(obj1[key])]);
-          arr.push(['+', key, obj2[key]]);
-        } else if ((typeof obj1[key] !== 'object') && (typeof obj2[key] === 'object') && (obj2[key] !== null)) {
-          arr.push(['-', key, obj1[key]]);
-          arr.push(['+', key, objToArr(obj2[key])]);
-        } else if (((typeof obj1[key] === 'object') && (typeof obj2[key] === 'object'))) {
-          arr.push([' ', key, getDiffs(obj1[key], obj2[key])]);
-        } else if (obj1[key] === obj2[key]) {
-          arr.push([' ', key, obj1[key]]);
-        } else {
-          arr.push(['-', key, obj1[key]]);
-          arr.push(['+', key, obj2[key]]);
-        }
-      } else if (in1 && !in2) {
-        if (typeof obj1[key] === 'object') {
-          arr.push(['-', key, objToArr(obj1[key])]);
-        } else {
-          arr.push(['-', key, obj1[key]]);
-        }
-      } else if (typeof obj2[key] === 'object') {
-        arr.push(['+', key, objToArr(obj2[key])]);
-      } else {
-        arr.push(['+', key, obj2[key]]);
-      }
-      return arr;
-    }, []);
-    return res;
-  }; */
-
   const getDiffs = (obj1, obj2) => {
-    const keys = _.uniq(_.concat(Object.keys(obj1), Object.keys(obj2))).sort();
+    const keys = [..._.uniq(_.concat(Object.keys(obj1), Object.keys(obj2)))].sort();
 
     const res = keys.reduce((arr, key) => {
       const in1 = _.has(obj1, key);
@@ -57,41 +18,28 @@ const getDiff = (filePath1, filePath2) => {
       if (in1 && in2) {
         if ((typeof obj1[key] === 'object') && (obj1[key] !== null) && (typeof obj2[key] !== 'object')) {
           return [...arr, ['-', key, objToArr(obj1[key])], ['+', key, obj2[key]]];
-          // arr.push(['-', key, objToArr(obj1[key])]);
-          // arr.push(['+', key, obj2[key]]);
         }
         if ((typeof obj1[key] !== 'object') && (typeof obj2[key] === 'object') && (obj2[key] !== null)) {
           return [...arr, ['-', key, obj1[key]], ['+', key, objToArr(obj2[key])]];
-          // arr.push(['-', key, obj1[key]]);
-          // arr.push(['+', key, objToArr(obj2[key])]);
         }
         if (((typeof obj1[key] === 'object') && (typeof obj2[key] === 'object'))) {
           return [...arr, [' ', key, getDiffs(obj1[key], obj2[key])]];
-          // arr.push([' ', key, getDiffs(obj1[key], obj2[key])]);
         }
         if (obj1[key] === obj2[key]) {
           return [...arr, [' ', key, obj1[key]]];
-          // arr.push([' ', key, obj1[key]]);
         }
         return [...arr, ['-', key, obj1[key]], ['+', key, obj2[key]]];
-        // arr.push(['-', key, obj1[key]]);
-        // arr.push(['+', key, obj2[key]]);
       }
       if (in1 && !in2) {
         if (typeof obj1[key] === 'object') {
           return [...arr, ['-', key, objToArr(obj1[key])]];
-          // arr.push(['-', key, objToArr(obj1[key])]);
         }
         return [...arr, ['-', key, obj1[key]]];
-        // arr.push(['-', key, obj1[key]]);
       }
       if (typeof obj2[key] === 'object') {
         return [...arr, ['+', key, objToArr(obj2[key])]];
-        // arr.push(['+', key, objToArr(obj2[key])]);
       }
       return [...arr, ['+', key, obj2[key]]];
-      // arr.push(['+', key, obj2[key]]);
-      // return arr;
     }, []);
     return res;
   };
