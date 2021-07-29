f828a2716e2442fa04e95e0deeb50468b105c504
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-29 03:43:35 -0700

    check 12

--- formatters/plain.js
+++ formatters/plain.js
@@ -7,62 +7,11 @@ const makeValue = (value) => {
   return typeof value === 'string' ? `'${value}'` : value;
 };
 
-/* export default (data) => {
-  const getResult = (dif, way, res) => {
-    dif.forEach((elem) => {
-      const localWay = way === '' ? elem[1] : `${way}.${elem[1]}`;
-      const value = makeValue(elem[2]);
-      if (elem[0] === '-') {
-        res[localWay] = {
-          change: '-',
-          oldValue: value,
-        };
-      } else if (elem[0] === '+') {
-        if (_.has(res, localWay)) {
-          res[localWay].change = '*';
-          res[localWay].newValue = value;
-        } else {
-          res[localWay] = {
-            change: '+',
-            newValue: value,
-          };
-        }
-      } else if (Array.isArray(elem[2])) {
-        getResult(elem[2], localWay, res);
-      }
-    });
-    return res;
-  };
-
-  const preparings = getResult(data, '', {});
-
-  return Object.keys(preparings).reduce((res, key) => {
-    const begin = `Property '${key}' was `;
-
-    if (preparings[key].change === '+') {
-      const mainInfo = `added with value: ${preparings[key].newValue}`;
-      return res === ''
-        ? begin.concat(mainInfo)
-        : res.concat('\n', begin.concat(mainInfo));
-    }
-
-    if (preparings[key].change === '-') {
-      return res === ''
-        ? begin.concat('removed')
-        : res.concat('\n', begin.concat('removed'));
-    }
-
-    const mainInfo = `updated. From ${preparings[key].oldValue} to ${preparings[key].newValue}`;
-    return res === ''
-      ? begin.concat(mainInfo)
-      : res.concat('\n', begin.concat(mainInfo));
-  }, '');
-}; */
-
 export default (data) => {
   const getResult = (dif, way) => dif.reduce((acc, elem) => {
     const localWay = way === '' ? elem[1] : `${way}.${elem[1]}`;
     const value = makeValue(elem[2]);
+
     if (elem[0] === '-') {
       return { ...acc, [localWay]: { change: '-', oldValue: value } };
     }
@@ -80,30 +29,6 @@ export default (data) => {
 
     return acc;
   }, {});
-
-  /* dif.forEach((elem) => {
-      const localWay = way === '' ? elem[1] : `${way}.${elem[1]}`;
-      const value = makeValue(elem[2]);
-      if (elem[0] === '-') {
-        res[localWay] = {
-          change: '-',
-          oldValue: value,
-        };
-      } else if (elem[0] === '+') {
-        if (_.has(res, localWay)) {
-          res[localWay].change = '*';
-          res[localWay].newValue = value;
-        } else {
-          res[localWay] = {
-            change: '+',
-            newValue: value,
-          };
-        }
-      } else if (Array.isArray(elem[2])) {
-        getResult(elem[2], localWay, res);
-      }
-    });
-    return res; */
 
   const preparings = getResult(data, '');
 
--- src/diff.js
+++ src/diff.js
@@ -12,7 +12,7 @@ const objToArr = (obj) => {
 };
 
 const getDiff = (filePath1, filePath2) => {
-  const getDiffs = (obj1, obj2) => {
+  /* const getDiffs = (obj1, obj2) => {
     const keys = _.uniq(_.concat(Object.keys(obj1), Object.keys(obj2))).sort();
     const res = keys.reduce((arr, key) => {
       const in1 = _.has(obj1, key);
@@ -44,6 +44,54 @@ const getDiff = (filePath1, filePath2) => {
         arr.push(['+', key, obj2[key]]);
       }
       return arr;
+    }, []);
+    return res;
+  }; */
+
+  const getDiffs = (obj1, obj2) => {
+    const keys = _.uniq(_.concat(Object.keys(obj1), Object.keys(obj2))).sort();
+
+    const res = keys.reduce((arr, key) => {
+      const in1 = _.has(obj1, key);
+      const in2 = _.has(obj2, key);
+      if (in1 && in2) {
+        if ((typeof obj1[key] === 'object') && (obj1[key] !== null) && (typeof obj2[key] !== 'object')) {
+          return [...arr, ['-', key, objToArr(obj1[key])], ['+', key, obj2[key]]];
+          // arr.push(['-', key, objToArr(obj1[key])]);
+          // arr.push(['+', key, obj2[key]]);
+        }
+        if ((typeof obj1[key] !== 'object') && (typeof obj2[key] === 'object') && (obj2[key] !== null)) {
+          return [...arr, ['-', key, obj1[key]], ['+', key, objToArr(obj2[key])]];
+          // arr.push(['-', key, obj1[key]]);
+          // arr.push(['+', key, objToArr(obj2[key])]);
+        }
+        if (((typeof obj1[key] === 'object') && (typeof obj2[key] === 'object'))) {
+          return [...arr, [' ', key, getDiffs(obj1[key], obj2[key])]];
+          // arr.push([' ', key, getDiffs(obj1[key], obj2[key])]);
+        }
+        if (obj1[key] === obj2[key]) {
+          return [...arr, [' ', key, obj1[key]]];
+          // arr.push([' ', key, obj1[key]]);
+        }
+        return [...arr, ['-', key, obj1[key]], ['+', key, obj2[key]]];
+        // arr.push(['-', key, obj1[key]]);
+        // arr.push(['+', key, obj2[key]]);
+      }
+      if (in1 && !in2) {
+        if (typeof obj1[key] === 'object') {
+          return [...arr, ['-', key, objToArr(obj1[key])]];
+          // arr.push(['-', key, objToArr(obj1[key])]);
+        }
+        return [...arr, ['-', key, obj1[key]]];
+        // arr.push(['-', key, obj1[key]]);
+      }
+      if (typeof obj2[key] === 'object') {
+        return [...arr, ['+', key, objToArr(obj2[key])]];
+        // arr.push(['+', key, objToArr(obj2[key])]);
+      }
+      return [...arr, ['+', key, obj2[key]]];
+      // arr.push(['+', key, obj2[key]]);
+      // return arr;
     }, []);
     return res;
   };
