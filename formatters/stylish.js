ec19e1c4fa0476bc1e0bf03454a0b318e86e79be
Dmitriy Nesterovich <abcd-xyz@list.ru>
2021-07-29 04:47:13 -0700

    project done

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -8,18 +8,5 @@ export default (data) => {
       : elem[2])
     .concat('\n'), '');
 
-  /* let str = '';
-    arr.forEach((elem) => {
-      str = str.concat(`${getOffset(level)}${elem[0]} ${elem[1]}: `);
-      if (Array.isArray(elem[2])) {
-        str = str.concat('{\n'.concat(out(elem[2], level + 2)));
-        str = str.concat(`${getOffset(level + 1)}}`);
-      } else {
-        str = str.concat(elem[2]);
-      }
-      str = str.concat('\n');
-    });
-    return str; */
-
   return `{\n${out(data, 1)}}`;
 };
